f8f9568e006c2460a5e634897070f8dec69d0df7
Orest Khomitskyi <orestkhomitskyi@Orests-MBP.Dlink>
2020-02-01 17:09:21 +0200

    max found

--- test.php
+++ test.php
@@ -5,4 +5,17 @@ require(__DIR__ . '/vendor/paralleldots/apis/autoload.php');
 $api_key = "UMdMIPiyi0xlKbivrG5Eahx68gscgK4DoAkclkrAmlw";
 
 set_api_key($api_key);
-echo facial_emotion("./sasha.jpg");
+
+
+
+$response_array = json_decode(facial_emotion("./neutral.jpg"), TRUE);
+
+
+$max = $response_array['facial_emotion'][0];
+for($i = 1; $i < count($response_array); $i++) {
+    if ($response_array[i]['score'] > $max['score'] ) {
+        $max = $response_array[i];
+    }
+}
+
+echo $max['tag'];
